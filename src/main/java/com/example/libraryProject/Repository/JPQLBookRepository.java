4acb75b9537da01d8fe0d4c9ef6250f010705879
kim-0zzy <kimdudtj3070@naver.com>
2023-12-26 16:26:44 +0900

    1226Commit

---
+++ src/main/java/com/example/libraryProject/Repository/JPQLBookRepository.java
@@ -0,0 +1,59 @@
+package com.example.libraryProject.Repository;
+
+import com.example.libraryProject.Entity.Book;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class JPQLBookRepository {
+
+    private final EntityManager em;
+
+    public void save(Book book){
+        em.persist(book);
+    }
+
+    public Book findById(Long id){
+        return em.find(Book.class, id);
+    }
+    public List<Book> findAll(){
+        return em.createQuery("select b from Book b", Book.class)
+                .getResultList();
+    }
+    public Book findByCode(String code) throws Exception{
+        try{
+            return em.createQuery("select b from Book b where b.code =: code"
+                            , Book.class)
+                    .setParameter("code", code)
+                    .getSingleResult();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public List<Book> findByBookName(String name) throws Exception{
+        try{
+            return em.createQuery("select b from Book b where b.name =: name"
+            , Book.class)
+                    .setParameter("name", name)
+                    .getResultList();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public List<Book> findByBookNameWithoutBorrowed(String name) throws Exception{
+        try{
+            return em.createQuery("select b from Book b where b.name =: name and b.state is true"
+            , Book.class)
+                    .setParameter("name", name)
+                    .getResultList();
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
